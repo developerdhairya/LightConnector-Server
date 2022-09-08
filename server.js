78db4b6857e3eb47b2f292f97d8f4ca2b2a56b20
developerdhairya <dhairya0192.be20@chitkara.edu.in>
2022-09-08 18:06:09 +0530

    revise

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
 const {Server}=require("socket.io");
 
 const io=new Server(3000,{cors: {
-    origin: "http://127.0.0.1:5500"
+    // origin: "http://localhost:5500"
   }});
 
-io.on("connection",(socket)=>{console.log(socket.id);});
+io.on("connection",(socket)=>{
+
+  socket.on("mobile-scan",(room)=>{
+    socket.join(room);
+    io.to(room).emit("transfer-ready");
+  })
+
+  socket.on("qr-generate",(room)=>{
+    socket.join(room);
+  });
+
+  socket.on("transfer",({room,message,file,device})=>{
+    console.log(room,message,file,device);
+    io.to(room).emit("receive",message,file,device);
+    console.log(1500);
+  })
+
+
+  
+
+});
 
