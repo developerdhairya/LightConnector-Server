028a7f763f65bcbfdb9752546be139b4301ee0f1
developerdhairya <dhairya0192.be20@chitkara.edu.in>
2022-10-27 21:43:04 +0530

    merge local

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const {Server}=require("socket.io");
 
-const io=new Server(3000,{cors: {
-    // origin: "http://localhost:5500"
-  }});
+const io=new Server(3000,{
+    maxHttpBufferSize: 1e800000000000000,
+    cors: {
+//     origin: "http://lightconnectornew.vercel.app"
+}});
 
 io.on("connection",(socket)=>{
 
   socket.on("mobile-scan",(room)=>{
     socket.join(room);
     io.to(room).emit("transfer-ready");
-  })
+  });
 
   socket.on("qr-generate",(room)=>{
     socket.join(room);
@@ -21,8 +23,6 @@ io.on("connection",(socket)=>{
     console.log(1500);
   })
 
-
-  
 
 });
 
